562d29b2ca1a2fadbfe13de56efe5eb375f33e59
Mr-Singhh <digvijaysinghpikku@gmail.com>
2022-07-18 16:46:49 +0530

    news app

---
+++ components/navbar.js
@@ -0,0 +1,18 @@
+function navbar() {
+    return `  <div id="navbar">
+    <h1 id="news"><a href="headlines">News</a> </h1>
+    <input type="text" id="search_input" placeholder="Search "> 
+
+</div>
+<div id="sidebar">
+    <h1 id="counteries">countries</h1>
+    <h3 id="in">India</h3>
+    <h3 id="ch">China</h3>
+    <h3 id="us">Usa</h3>
+    <h3 id="uk">UK</h3>
+    <h3 id="nz">Newzeland</h3>
+</div>`
+
+
+}
+export { navbar }
